2ebe263c7228f8b84c5b563962fdd96ce10df7ba
Polo08816 <kkuo12887@gmail.com>
2018-04-22 16:43:51 -0400

    Adding LED test file.

---
+++ Tutorials/TestLED.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+
+#pin definitions
+ledPin = 22
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(ledPin, GPIO.OUT)
+
+print("LED On!")
+
+GPIO.output(ledPin, GPIO.HIGH)
+time.sleep(6)
+
+print("LED Off!")
+GPIO.output(ledPin, GPIO.LOW)
+
+GPIO.cleanup()
