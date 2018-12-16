d94b4763fedb5823b1838a4152d1ffa1d667a62e
Kevin Kuo <Kevin Kuo@DESKTOP-93T0KNQ>
2018-12-15 22:20:29 -0500

    Added development notes and logic for light switch.

--- Source Code/testLightSensorMovingAverages.py
+++ Source Code/testLightSensorMovingAverages.py
@@ -1,15 +1,9 @@
 import RPi.GPIO as GPIO
 import time
-
-import sys
-
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
-    
 import numpy as np
 import pandas as pd
+
+
 
 
 #pin definitions for outputs
@@ -39,31 +33,22 @@ def RCtime (RCpin):
     GPIO.setup(RCpin, GPIO.OUT)
     GPIO.output(RCpin, GPIO.LOW)
         
-    time.sleep(0.8)
+    time.sleep(0.5)
     GPIO.setup(RCpin, GPIO.IN)
     while (GPIO.input(RCpin) == GPIO.LOW):
         reading += 1
     return reading
     
+# note that the lower the lightReading, the brighter the light
 while True:
         
         lightReading = RCtime(lightSensorPin)
         print(lightReading)
-        if (lightReading < 800):
+        if (lightReading < lightActivationThreshold):
             print("LED On!")
             GPIO.output(ledPin, GPIO.HIGH)
-            print("Turn on switch.")
-            # response = urllib2.urlopen('http://localhost:8083/ZWaveAPI/Run/devices[2].instances[0].commandClasses[0x25].Set(255)')
-            # html = response.read()
-            # print(html)
         else:
             print("LED Off!")
             GPIO.output(ledPin, GPIO.LOW)
-            print("Turn off switch.")
-            # response = urllib2.urlopen('http://localhost:8083/ZWaveAPI/Run/devices[2].instances[0].commandClasses[0x25].Set(0)')
-            # html = response.read()
-            # print(html)
         
-        
-GPIO.output(ledPin, GPIO.LOW)
 GPIO.cleanup()
